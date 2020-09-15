173faa681bf9e7832e0c147a110ece69c58cb38d
Matthew Teasdale <teasdam@gmail.com>
2020-09-15 17:04:01 +0100

    update cutadapt multi

--- cutadapt_multi.sh
+++ cutadapt_multi.sh
@@ -1,2 +1,13 @@
-# Matthew Teasdale
-# Cambridge 2020
+#! /usr/bin/env bash
+
+while read line
+do
+	base=${line%.*.*}
+	full=$base"_cutadapt.fastq.gz"
+	cutadapt \
+	--cores=10 \
+	--overlap=1 \
+	--minimum-length=30 \
+	--adapter=AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC \
+	--output=$full $line
+done
